d845605f636ff21f54c85730640953c9101c28f8
Abdulloh-Sultonmurodov <sultonmurodovabdulloh568@gmail.com>
2025-09-27 19:58:24 +0500

    add loading

--- app/[locale]/loading.tsx
+++ app/[locale]/loading.tsx
@@ -1,6 +1,10 @@
+import Image from "next/image";
+
 const loading = () => {
   return (
-    <div className="text-white h-[100vh] text-center mt-[250px]">loading</div>
+    <div className="text-white h-[100vh] flex items-center justify-center">
+      <Image src={"/loading.png"} alt="Loading img" width={50} height={50} />
+    </div>
   );
 };
 
